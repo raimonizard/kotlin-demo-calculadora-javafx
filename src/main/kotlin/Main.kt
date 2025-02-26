fd7544400a7a92b84977949f4c1fa45bea1b227e
raimonizard <raimon.izard@gmail.com>
2025-02-26 08:16:54 +0100

    feat: add menu with screen option - not finished

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,38 @@
 import javafx.application.Application
 import org.example.CalculadoraJavaFXAPI
 import util.readDouble
+import util.readInt
 import view.CalculadoraTerminal
 
 fun main() {
-    /* Cridem a la vista de JavaFX API */
-    Application.launch(CalculadoraJavaFXAPI::class.java)
+    do {
+        var opcioGui: Int = readInt(
+            "Escull vista:\n\t1. Terminal\n\t2. GUI\n\t3. Sortir",
+            "Valor incorrecte",
+            "Valor fora de rang",
+            1,
+            3
+        )
+        when (opcioGui) {
+            1 -> calculadoraVistaTerminal(calculadora)
+            2 -> calculadoraVistaGUI()
+            3 -> println("Sortint...")
+        }
+    }while (opcioGui != 3)
+}
 
+fun calculadoraVistaTerminal(calculadora: Calculadora) {
     /* Cridem a la vista del terminal de IntelliJ */
-    val calculadoraTerminal: CalculadoraTerminal = CalculadoraTerminal()
 
-    calculadoraTerminal.setX(readDouble("Escriu el valor del primer operand", "Valor incorrecte"))
-    calculadoraTerminal.setY(readDouble("Escriu el valor del segon operand", "Valor incorrecte"))
+    calculadora.setX(readDouble("Escriu el valor del primer operand", "Valor incorrecte"))
+    calculadora.setY(readDouble("Escriu el valor del segon operand", "Valor incorrecte"))
+
+    val calculadoraTerminal: CalculadoraTerminal = CalculadoraTerminal(calculadora)
     calculadoraTerminal.escollirOperacio()
-    println(calculadoraTerminal)
+    println(calculadoraTerminal.getCalculadora())
+}
+
+fun calculadoraVistaGUI() {
+    /* Cridem a la vista de JavaFX API */
+    Application.launch(CalculadoraJavaFXAPI()::class.java)
 }
