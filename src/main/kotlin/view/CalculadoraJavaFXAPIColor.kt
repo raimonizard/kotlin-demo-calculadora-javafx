345ae68a46aa654cc47ac1ebb7fa5404466ff890
raimonizard <raimon.izard@gmail.com>
2025-02-26 12:35:33 +0100

    feat: add view CalculadoraJavaFXAPIColor and Color ENUM - stable version

--- src/main/kotlin/view/CalculadoraJavaFXAPIColor.kt
+++ src/main/kotlin/view/CalculadoraJavaFXAPIColor.kt
@@ -5,6 +5,7 @@ import javafx.geometry.Insets
 import javafx.stage.Stage
 import javafx.scene.Scene
 import javafx.scene.control.Button
+import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.layout.HBox
@@ -16,7 +17,7 @@ import org.example.utils.CalculadoraController.multi
 import org.example.utils.CalculadoraController.resta
 import org.example.utils.CalculadoraController.suma
 
-class CalculadoraJavaFXAPI: Application() {
+class CalculadoraJavaFXAPIColor: Application() {
 
     override fun start(primerStage: Stage) {
         /* Declarem els atributs de la vista */
@@ -27,6 +28,10 @@ class CalculadoraJavaFXAPI: Application() {
         val btnMulti = Button(TipusOperacio.MULTIPLICACIO.simbol)
         val btnDiv = Button(TipusOperacio.DIVISIO.simbol)
         val lblResultat = Label()
+        // Desplegable 'DropDown' per escollir el color de la GUI
+        val desplegableColorBackground = ComboBox<Color>()
+        // Afegim al desplegable tots els colors definits dins de l'ENUM Color
+        desplegableColorBackground.items.addAll(Color.values())
 
         /* Definim el comportament de les accions de l'acció clickar damunt de cadascun dels botons */
 
@@ -39,17 +44,26 @@ class CalculadoraJavaFXAPI: Application() {
         btnResta.setOnAction {
             val valors = capturarValorsTextField(txtPrimerOperador, txtSegonOperador)
             val resultat: Double = resta(valors.first, valors.second)
-            lblResultat.text = "La ${TipusOperacio.RESTA.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"        }
+            lblResultat.text = "La ${TipusOperacio.RESTA.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"
+        }
 
         btnMulti.setOnAction {
             val valors = capturarValorsTextField(txtPrimerOperador, txtSegonOperador)
             val resultat: Double = multi(valors.first, valors.second)
-            lblResultat.text = "La ${TipusOperacio.MULTIPLICACIO.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"        }
+            lblResultat.text = "La ${TipusOperacio.MULTIPLICACIO.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"
+        }
 
         btnDiv.setOnAction {
             val valors = capturarValorsTextField(txtPrimerOperador, txtSegonOperador)
             val resultat: Double = div(valors.first, valors.second)
-            lblResultat.text = "La ${TipusOperacio.DIVISIO.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"        }
+            lblResultat.text = "La ${TipusOperacio.DIVISIO.toString().lowercase()} de ${valors.first} i ${valors.second} és: $resultat"
+        }
+
+        // Definim l'acció a fer quan s'interactuï amb el desplegable
+        desplegableColorBackground.setOnAction {
+            val colorTriat = desplegableColorBackground.value
+            primerStage.scene.root.style = "-fx-background-color: ${colorTriat.backgroundColor};"
+        }
 
         /* DEFINIM ELS COMPONENTS DE LA VISTA */
 
@@ -69,6 +83,8 @@ class CalculadoraJavaFXAPI: Application() {
         val vBox = VBox(10.0)
         vBox.padding = Insets(20.0) // Aquesta línia estableix un marge intern (padding) de 20 píxels a tots els costats del contenidor VBox.
         vBox.children.addAll(
+            Label("Escull skin de color de fons"),
+            desplegableColorBackground,
             Label("Introdueix el primer número:"),
             txtPrimerOperador,
             Label("Introdueix el segon número:"),
@@ -76,9 +92,11 @@ class CalculadoraJavaFXAPI: Application() {
             filaButonsOperacions, // Li afegim tots els elements de l'horitzontal box que hem creat abans
             lblResultat
         )
+        // Apliquem estil al contenidor vertical
+        //vBox.style = "-fx-background-color: ${Color.CYAN.backgroundColor};"
 
         // Definim la finestra emergent amb el seu contingut vbox i la seva mida en píxels horitzontal i vertical
-        val scene = Scene(vBox, 300.0, 250.0)
+        val scene = Scene(vBox, 400.0, 300.0)
         // Definim el títol que es mostrarà a la finestra emergent
         primerStage.title = "Calculadora"
         // Li assignem l'escena creada a l'atribut scene a la finestra GUI del nostre programa 'primerStage'
