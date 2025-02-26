9e633f3aa879f8e7d1faae394a121fa130ce6c1d
raimonizard <raimon.izard@gmail.com>
2025-02-26 08:30:24 +0100

    feat: add double view menu selector - stable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
 import javafx.application.Application
+import model.Calculadora
 import org.example.CalculadoraJavaFXAPI
 import util.readDouble
 import util.readInt
 import view.CalculadoraTerminal
 
+/**
+ * Entry point de l'aplicació.
+ * @author RIS
+ */
 fun main() {
+    val calculadora: Calculadora = Calculadora()
+
     do {
         var opcioGui: Int = readInt(
             "Escull vista:\n\t1. Terminal\n\t2. GUI\n\t3. Sortir",
@@ -21,18 +28,37 @@ fun main() {
     }while (opcioGui != 3)
 }
 
+/**
+ * Mètode usat per a inicialitzar els valors de l'objecte calculadora,
+ * declarar i inicialitzar l'objecte de la vista del terminal.
+ * @author RIS
+ * @param calculadora
+ */
 fun calculadoraVistaTerminal(calculadora: Calculadora) {
-    /* Cridem a la vista del terminal de IntelliJ */
-
+    /* Donem valors als atributs de l'Objecte Calculadora usant els seus setters */
     calculadora.setX(readDouble("Escriu el valor del primer operand", "Valor incorrecte"))
     calculadora.setY(readDouble("Escriu el valor del segon operand", "Valor incorrecte"))
 
+    /* Inicialitzem la vista de Calculadora Terminal */
     val calculadoraTerminal: CalculadoraTerminal = CalculadoraTerminal(calculadora)
+    /* Cridem a la vista del terminal de IntelliJ */
     calculadoraTerminal.escollirOperacio()
     println(calculadoraTerminal.getCalculadora())
 }
 
+/**
+ * Mètode que genera la finestra emergent de JavaFX API.
+ * Fa gestió d'excepcions ja que si es tanca la finestra emergent,
+ * no es podrà tornar a mostrar mentre el programa està en execució.
+ * @author RIS
+ */
 fun calculadoraVistaGUI() {
-    /* Cridem a la vista de JavaFX API */
-    Application.launch(CalculadoraJavaFXAPI()::class.java)
+    try {
+        /* Cridem a la vista de JavaFX API */
+        Application.launch(CalculadoraJavaFXAPI()::class.java)
+    }catch (ise: IllegalStateException){
+        println("ERROR: Si es tanca finestra de la GUI no es pot llançar més d'un cop mentre la app es troba en execució. S'hauria de generar una nova scene.")
+    }catch(e: Exception){
+        println("FATAL ERROR: ${e.stackTraceToString()}")
+    }
 }
--- src/main/kotlin/view/CalculadoraTerminal.kt
+++ src/main/kotlin/view/CalculadoraTerminal.kt
@@ -1,66 +1,36 @@
 package view
 
+import model.Calculadora
 import model.TipusOperacio
-import org.example.utils.CalculadoraController.div
-import org.example.utils.CalculadoraController.multi
-import org.example.utils.CalculadoraController.resta
-import org.example.utils.CalculadoraController.suma
 import util.*
 
 class CalculadoraTerminal {
-    private var x: Double
-    private var y: Double
-    lateinit var operacio: TipusOperacio
-    private var resultat: Double
+    private val calculadora: Calculadora
 
-    constructor() {
-        this.x = 0.0
-        this.y = 0.0
-        this.resultat = 0.0
+    /**
+     * Constructor de la classe de la vista
+     * @param calculadora: Inicialitza l'atribut calculadora
+     * @author RIS
+     */
+    constructor(calculadora: Calculadora) {
+        this.calculadora = calculadora
     }
 
-    constructor(x: Double, y: Double) {
-        this.x = x
-        this.y = y
-        this.resultat = 0.0
+    /**
+     * Mètode usat per tal de retornar l'atribut calculadora
+     * per poder accedir als mètodes de l'objecte.
+     * @author RIS
+     * @return calculadora
+     */
+    fun getCalculadora(): Calculadora {
+        return this.calculadora
     }
 
-    fun getX(): Double {
-        return this.x
-    }
-
-    fun setX(x: Double) {
-        this.x = x
-    }
-
-    fun getY(): Double {
-        return this.y
-    }
-
-    fun setY(y: Double) {
-        this.y = y
-    }
-
-    fun sumar(){
-        this.operacio = TipusOperacio.SUMA
-        this.resultat = suma(this.x, this.y)
-    }
-
-    fun restar(){
-        this.operacio = TipusOperacio.RESTA
-        this.resultat = resta(this.x, this.y)
-    }
-
-    fun multiplicar(){
-        this.operacio = TipusOperacio.MULTIPLICACIO
-        this.resultat = multi(this.x, this.y)
-    }
-
-    fun dividir(){
-        this.operacio = TipusOperacio.DIVISIO
-        this.resultat = div(this.x, this.y)
-    }
-
+    /**
+     * Mètode principal de la vista del temrinal de IntelliJ.
+     * Manté un diàleg amb l'usuari de la app via terminal.
+     * @author RIS
+     */
     fun escollirOperacio(){
         do {
             // Mostrem les operacions disponibles
@@ -74,18 +44,12 @@ class CalculadoraTerminal {
             val tipusOperacio = TipusOperacio.entries.find { it.name == operacio }
 
             when (tipusOperacio) {
-                TipusOperacio.SUMA -> this.sumar()
-                TipusOperacio.RESTA -> this.restar()
-                TipusOperacio.MULTIPLICACIO -> this.multiplicar()
-                TipusOperacio.DIVISIO -> this.dividir()
+                TipusOperacio.SUMA -> calculadora.sumar()
+                TipusOperacio.RESTA -> calculadora.restar()
+                TipusOperacio.MULTIPLICACIO -> calculadora.multiplicar()
+                TipusOperacio.DIVISIO -> calculadora.dividir()
                 else -> println(RED + "ERROR: Operació no vàlida" + RESET)
             }
         } while (tipusOperacio == null)
     }
-
-    override fun toString(): String {
-        return "CalculadoraTerminal(${this.x} ${this.operacio.simbol} ${this.y} = ${this.resultat})"
-    }
-
-
 }
